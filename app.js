58859c537b66676eb32115f4ffa429eb2d797ef0
daffaaptar <daffaaptapratama@gmail.com>
2023-07-24 12:50:28 +0700

    video kedua

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+const yargs = require("yargs");
 const contacts = require('./contacts');
 
-const main = async () => {
-  const nama = await contacts.tulispertanyaan('Masukan nama anda :');
-  const email = await contacts.tulispertanyaan('Masukan email anda :');
-  const noHP = await contacts.tulispertanyaan('Masukan no HP anda :');
-  
-  contacts.simpanContact(nama, email, noHP);
-};
+yargs.command({
+  command: 'add',
+  describe: 'Menambahkan contact baru',
+  builder: {
+    nama: {
+      describe: 'Nama Lengkap',
+      demandOption: true,
+      type: 'string',
+    },
+    email: {
+      describe: 'Email',
+      demandOption: false,
+      type: 'string',
+    },
+    noHP: {
+      describe: 'Nomor Handphone',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv){
+    contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+  },
+});
 
-main();
+yargs.parse();
--- contacts.js
+++ contacts.js
@@ -1,12 +1,5 @@
 const { rejects } = require('assert');
 const fs = require('fs');
-const { resolve } = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 // membuat folder data jika belum ada
 const dirpath = './data';
@@ -20,25 +13,24 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulispertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject)=> {
-rl.question(pertanyaan, (nama) => {
-   resolve(nama);
-    });
-  });
-};
 
 const simpanContact = (nama, email, noHP) => {
     const contact = { nama, email, noHP };
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(fileBuffer);
 
+    //cek duplikat
+    const duplikat = contacts.find ((contact) => contact.nama === nama);
+    if (duplikat) {
+      console.log('Contact sudah terdaftar, gunakan nama lain!')
+      return false;
+    }
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
     console.log('terimakasih sudah memasukkan data.');
-    rl.close();
 };
 
-module.exports = { tulispertanyaan, simpanContact}
+module.exports = {simpanContact}
